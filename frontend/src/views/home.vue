e4da637e61ec2f33632b535220290390540f588c
lrbwhite <3523395067@qq.com>
2026-09-25 12:09:56 +0000

    修改

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>这是测试语句</div>
+  <div class="common-layout">
+    <el-container style="min-height: 100vh;">
+      <el-header style="display: flex; border-bottom: 1px solid #ddd">
+        <div style="display: flex; align-items:center">实验室预约系统</div>
+      </el-header>
+      <el-container>
+        <el-aside width="220px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
